9116dead145fec61e628c89566c9d144a035bcbd
Mario Guerra <mgg7007@gmail.com>
2019-08-16 15:22:23 +0200

    Disposición inicial.

---
+++ Program.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Textuo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Silab (Proyecto 'Textuo') v1.0");
+                Console.WriteLine("  Divide una palabra en las sílabas que la forman, siguiendo las reglas");
+                Console.WriteLine("  ortográficas del Español.");
+
+                Console.WriteLine();
+                Console.WriteLine("  Uso:");
+                Console.WriteLine("    Silab <Palabra> : Muestra las sílabas que componen <Palabra>.");
+                Console.WriteLine();
+                Console.WriteLine("    Silab -f <Archivo>");
+                Console.WriteLine("    Silab --file <Archivo>");
+                Console.WriteLine("        Muestra las sílabas de cada palabra que se encuentre en el");
+                Console.WriteLine("        <Archivo>. Debe aparecer una palabra por línea.");
+                Console.WriteLine();
+                Console.WriteLine("    Silab -i");
+                Console.WriteLine("    Silab --in");
+                Console.WriteLine("        Muestra las sílabas de cada palabra que se encuentre en la");
+                Console.WriteLine("        entrada estándar (stdin).");
+                Console.WriteLine();
+            }
+            else if(args.Length > 0)
+            {
+                var arg = args[0].ToLower();
+
+                if(arg.StartsWith("--") && arg.Length > "--".Length)
+                {
+                    if(arg == "--file")
+                    {
+                        if(args.Length > 1)
+                            LeerArchivo(args[1]);
+                        else
+                            ErrorArchivoNoEspecificado();
+                    }
+                    else if(arg == "--in")
+                        LeerEntrada();
+                    else
+                        ErrorArgumentoNoVálido(args[0]);
+                }
+                else if(arg.StartsWith("-") && arg.Length > "-".Length)
+                {
+                    if(arg == "-f")
+                    {
+                        if(args.Length > 1)
+                            LeerArchivo(args[1]);
+                        else
+                            ErrorArchivoNoEspecificado();
+                    }
+                    else if(arg == "-i")
+                        LeerEntrada();
+                    else
+                        ErrorArgumentoNoVálido(args[0]);
+                }
+                else
+                {
+                    // Mostrar las sílabas de una sola palabra
+                    MostrarSílabas(arg);
+                }
+            }
+        }
+
+        #region Interfaz de usuario
+
+        //
+        // Divide una palabra en sus sílabas y las muestra.
+        //
+        private static void MostrarSílabas(string palabra)
+        {
+            var sílabas = DividirEnSílabas(palabra);
+            Array.Reverse(sílabas);
+
+            Console.WriteLine( string.Join("-", sílabas) );
+        }
+
+        //
+        // Lee un archivo y divide en sílabas cada palabra que aparezca. El archivo se lee línea a línea.
+        //
+        private static void LeerArchivo(string archivo)
+        {
+            if(!File.Exists(archivo))
+                ErrorArchivoNoEncontrado(archivo);
+            else
+            {
+                try
+                {
+                    // Redirigimos a la entrada estándar el contenido del archivo
+                    Console.SetIn(new StreamReader(archivo));
+
+                    // Leemos por la entrada estándar normalmente
+                    LeerEntrada();
+                }
+                catch(IOException)
+                {
+                    // Ante cualquier error asumimos que el archivo no se puede leer
+                    ErrorArchivoNoSePuedeAbrir(archivo);
+                }
+            }
+        }
+
+        //
+        // Divide en sílabas cada palabra que aparezca en la entrada estándar.
+        //
+        private static void LeerEntrada()
+        {
+            string línea;
+            while ((línea = Console.ReadLine()) != null)
+            {
+                MostrarSílabas(línea);
+            }
+        }
+
+        // === Mensajes de error === === === === === === === === === === === === === === //
+
+        //
+        // Error: Argumento no válido.
+        //
+        private static void ErrorArgumentoNoVálido(string arg)
+        {
+            MostrarError($"El argumento '{arg}' no es válido");
+        }
+
+        //
+        // Error: Archivo no especificado.
+        //
+        private static void ErrorArchivoNoEspecificado()
+        {
+            MostrarError("No se ha especificado el nombre de archivo a leer", 2);
+        }
+
+        //
+        // Error: Archivo no encontrado.
+        //
+        private static void ErrorArchivoNoEncontrado(string archivo)
+        {
+            MostrarError($"El archivo '{archivo}' no existe", 3);
+        }
+
+        //
+        // Error: No se puede abrir el archivo.
+        //
+        private static void ErrorArchivoNoSePuedeAbrir(string archivo)
+        {
+            MostrarError($"El archivo '{archivo}' no se puede leer", 4);
+        }
+
+        //
+        // Muestra un mensaje de error y sale con un código de error.
+        //
+        private static void MostrarError(string mensaje, int código = 1)
+        {
+            TextWriter errorWriter = Console.Error;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            errorWriter.WriteLine();
+            errorWriter.WriteLine($"  ERROR: {mensaje}");
+            errorWriter.WriteLine();
+
+            Console.ResetColor();
+            Environment.Exit(exitCode: código);
+        }
+
+        #endregion
+
+        const char LETRA_NO_VÁLIDA = '\0';
+
+        private enum Vocal
+        {
+            Ninguna,
+            Cerrada,
+            Abierta,
+        }
+
+        private static string[] DividirEnSílabas(string palabra)
+        {
+            if (string.IsNullOrWhiteSpace(palabra))
+                return Array.Empty<string>();
+
+            palabra = palabra.Trim();
+
+            char letra;
+            char letraSig = LETRA_NO_VÁLIDA;
+
+            var listaSílabas = new List<string>();
+
+            int numVocales = 0;
+            string sílaba = "";
+
+            for (int i = palabra.Length - 1; i >= 0; --i)
+            {
+                bool terminada = false;
+
+                letra = palabra[i];
+
+                bool esVocal = EsVocal(letra, out Vocal tipoVocal);
+                bool esVocalSig = EsVocal(letraSig, out Vocal tipoVocalSig);
+
+                string letraCombi = letra + (letraSig != LETRA_NO_VÁLIDA ? letraSig.ToString() : "");
+
+                if (esVocal && !esVocalSig)
+                    numVocales++;
+
+                if (numVocales > 1 && !esVocalSig)
+                    terminada = true;
+                else if (!esVocal && !esVocalSig && !EsGrupoConsonántico(letraCombi) && (numVocales == 1 && i > 1))
+                    terminada = true;
+                else if ((tipoVocal == Vocal.Abierta && tipoVocalSig == Vocal.Abierta) ||
+                        (tipoVocal == Vocal.Cerrada && TieneTilde(letra) && esVocalSig) ||
+                        (tipoVocal == Vocal.Abierta && (tipoVocalSig == Vocal.Cerrada && TieneTilde(letraSig))) ||
+                        ((esVocal && esVocalSig) && (letra == letraSig)))
+                {
+                    terminada = true;
+                    numVocales++;
+                }
+
+                if (terminada)
+                {
+                    listaSílabas.Add(sílaba);
+                    sílaba = letra.ToString();
+                    numVocales--;
+                }
+                else
+                    sílaba = letra + sílaba;
+
+                letraSig = letra;
+            }
+
+            listaSílabas.Add(sílaba);
+
+            return listaSílabas.ToArray();
+        }
+
+        private static bool TieneTilde(char letra)
+        {
+            letra = char.ToLower(letra);
+        
+            switch(letra)
+            {
+                case 'á':
+                case 'é':
+                case 'í':
+                case 'ó':
+                case 'ú':
+                    return true;
+                
+                default:
+                    return false;
+            }
+        }
+
+        private static bool EsVocal(char letra, out Vocal tipoDeVocal)
+        {
+            if(letra != LETRA_NO_VÁLIDA)
+            {
+                tipoDeVocal = TipoDeVocal(letra);
+                return EsVocal(letra);
+            }
+
+            tipoDeVocal = Vocal.Ninguna;
+            return false;
+        }
+
+        private static bool EsVocal(char letra)
+        {
+            if(letra != LETRA_NO_VÁLIDA)
+            {
+                switch(letra)
+                {
+                    case 'a':
+                    case 'á':
+                    case 'e':
+                    case 'é':
+                    case 'i':
+                    case 'í':
+                    case 'o':
+                    case 'ó':
+                    case 'u':
+                    case 'ú':
+                    case 'ü':
+                        return true;
+                    
+                    default:
+                        return false;
+                }
+            }
+            
+            return false;
+        }
+
+        private static Vocal TipoDeVocal(char letra)
+        {
+            if(letra != LETRA_NO_VÁLIDA)
+            {
+                if(EsVocal(letra))
+                {
+                    switch(letra)
+                    {
+                        case 'i':
+                        case 'í':
+                        case 'u':
+                        case 'ú':
+                        case 'ü':
+                            return Vocal.Cerrada;
+                        
+                        default:
+                            return Vocal.Abierta;
+                    }
+                }
+            }
+            
+            return Vocal.Ninguna;
+        }
+
+        private static bool EsLetra(char letra)
+        {
+            if(letra >= 'a' && letra <= 'z' || letra == 'ñ')
+                return true;
+            if(letra == 'á' || letra == 'é' || letra == 'í' || letra == 'ó' || letra == 'ú' || letra == 'ü')
+                return true;
+            
+            return false;
+        }
+
+        private static bool EsGrupoConsonántico(string letras)
+        {
+            letras = letras.ToLower();
+
+            switch(letras)
+            {
+                case "bl":
+                case "br":
+                case "cl":
+                case "cr":
+                case "dl":
+                case "dr":
+                case "fl":
+                case "fr":
+                case "gl":
+                case "gr":
+                case "pl":
+                case "pr":
+                case "tl":
+                case "tr":
+                case "kl":
+                case "kr":
+                case "ll":
+                case "rr":
+                case "ch":
+                case "qu":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ Reglas.md
@@ -0,0 +1,149 @@
+Reglas de la división silábica
+==============================
+
+Reglas básicas
+--------------
+
+Todas las vocales pueden formar sílabas por sí mismas.
+En este grupo se incluyen los monosílabos.
+
+Ejemplos:
+    a-pren-der, u-sur-par
+
+Todas las sílabas también deben estar compuestas al menos por una vocal.
+
+Una consonante o más escrita al inicio de la palabra forma sílaba con la vocal posterior.
+
+Ejemplos:
+    me-sa, si-lla, co-mer
+
+Así mismo, toda consonante o secuencia de consonante que se encuentre al final se une con la vocal anterior para formar sílaba.
+
+Ejemplos:
+    a-zul, ca-mi-sa
+
+Cuando una consonante se encuentra entre dos vocales, forma sílaba con la vocal siguiente.
+
+Ejemplos:
+    é-po-ca
+
+Casos especiales
+----------------
+
+Cuando son dos las consonantes que estén en entre dos vocales, se puede dar dos casos, los cuales son:
+
+ 1. El grupo constituido por br, cr, dr, gr, fr, kr, tr y bl, cl, gl, fl, kl, pl
+    es siempre inseparable y forma sílaba con la vocal que les sigue.
+
+    Ejemplos:
+        co-fre, la-drón, pla-to, fran-cés
+
+
+ 2. Prefijos y de composición:
+    En algunos casos, concretamente cuando hay prefijos y composición. Si un prefijo productivo (ej. sub-, post-) se antepone una palabra que inicia con l o r, o bien, se trata de una palabra terminada en algunas consonantes citadas más arriba, la l o la r no se suelen unir con la consonante anterior, sino que forman sílaba con la vocal posterior.
+
+    Ejemplos:
+        sub-ra-yar, sub-lu-nar, post-ro-mán-ti-co
+
+
+ 3. Secuencia tl:
+    La secuencia tl, en mayor parte de España, y también en algunos países de América se pronuncia en sílabas distintas. Sin embargo, en algunas regiones, como en Canarias, o en México, se pronuncia en una sola sílaba. De esta forma, las palabras con la secuencia tl son susceptibles de separarse en sílabas de dos maneras distintas, dependiendo de la región de su uso.
+
+    Ejemplos:
+        atleta= at-le-ta; o a-tle-ta
+
+
+ 4. Demás grupos consonánticos
+    Las demás secuencias consonánticas pertenecen a sílabas distintas.
+
+    Ejemplos:
+        hip-no-sis, ac-to, pes-car
+
+
+ 5. Sílabas con tres consonantes entre vocales:
+    Si se trata de tres consonantes en posición intervocálicas, las dos primeras consonantes se pronuncian en una misma sílaba que van unidas a la sílaba anterior, y la última forma parte de la sílaba siguiente.
+
+    Ejemplos:
+        ins-tau-rar, pers-pi-caz
+
+    Sin embargo, si las últimas dos consonantes pertenecen a los grupos citados anteriormente (pr, br, tr, bl, gl, etc) la separación en sílabas se realiza teniendo la regla para esos casos (ej.es-plen-dor).
+
+
+ 6. Sílabas con cuatro consonantes entre vocales:
+    Las dos primeras consonantes aparecen como el final de una sílaba, mientras que las restantes dos forman parte de la sílaba posterior.
+
+    Ejemplos:
+        abs-trac-to
+
+-----------------------------------------------------------------------------------------------------------
+
+ I. Una consonante entre vocales se une con la vocal posterior.
+
+    ala ==> a – la
+    oro ==> o – ro
+    amor ==> a – mor
+    humo ==> hu – mo
+
+ II. Si hay dos consonantes entre vocales, la primera consonante se une con la vocal anterior y la segunda con la posterior.
+
+     alto ==> al – to
+     granja ==> gran – ja
+     campo ==> cam – po
+     santo ==> san – to
+
+     Si ambas consonantes forman un grupo consonántico se unen a la vocal posterior.
+
+     potro ==> po – tro
+     aprecio ==> a – pre – cio
+     ogro ==> o – gro
+     aplasto ==> a – plas – to
+
+ III. Si hay tres consonantes entre vocales, las dos primeras consonantes se unen a la vocal anterior, y la tercera consonante a la vocal posterior.
+
+      instinto ==> ins – tin – to
+      perspectiva ==> pers – pec – ti –va
+      istmo ==> ist – mo
+      abstinencia ==> abs – ti – nen – cia
+      obstar ==> obs – tar
+      instar ==> ins – tar
+
+      Si de las tres consonantes, las dos ultimas forman un grupo consonántico, se unen a la vocal posterior.
+
+      estrella ==> es – tre – lla
+      hambre ==> ham – bre
+      amplio ==> am – plio
+      compra ==> com – pra
+      conflicto ==> con – flic – to
+      discreto ==> dis – cre – to
+
+ IV. Cuando concurren cuatro consonantes entre vocales, las dos primeras se unen a la vocal anterior y las dos ultimas, que siempre constituyen grupo consonántico, a la vocal posterior.
+
+     conscripción ==> cons – crip – ción
+     instructor ==> ins – truc – tor
+     instruir ==> ins – truir
+     construir ==> cons – truir
+
+ V. Las vocales que concurren sin constituir diptongo ni triptongo se separan en sílabas distintas.
+
+    óleo ==> ó – le – o
+    oasis ==> o – a –sis
+    diríais ==> di – rí – ais
+    aseo ==> a – se – o
+    bujía ==> bu – jí – a
+    héroe ==> hé – ro – e
+
+ VI. Las voces con prefijos admiten doble forma de silabeo: separando sus elementos componentes o siguiendo las reglas anteriores.
+
+     nosotros ==> nos – o – tros ==> no – so – tros
+     vosotros ==> vos – o – tros ==> vo – so – tros
+     inútil ==> in – ú – til ==> i – nú – til
+     inestable ==> in – es – ta – ble ==> i – nes – ta – ble
+
+     Nunca debe dejarse sola una vocal, aunque por sí misma forme sílaba, al final del renglón.
+
+ VII. La H y la X entre vocales se unen a la vocal posterior.
+
+      examen ==> e – xa – men
+      éxito ==> é – xi – to
+      ahorro ==> a – ho – rro
+      alcohol ==> al – co – hol
